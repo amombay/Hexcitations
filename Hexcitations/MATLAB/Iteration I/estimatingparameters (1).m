eac0848fee285259e327eaef0ec467f39e8c5727
amombay <andrew_mombay@brown.edu>
2025-12-12 17:32:12 -0500

    Add files via upload

---
+++ Hexcitations/MATLAB/Iteration I/estimatingparameters (1).m
@@ -0,0 +1,16 @@
+%% Zheng parameters 
+% (they never say what like anything is so im estimating some of the values;
+
+L_silicone = [2.5*4.4]; %In mm, looks roughly 2.5x the width.
+G = 0.23*(10)^6; % in Pa
+W = 4.4; %in mm
+H = 2;%in mm
+w = W/2; %for computing J
+h = H/2; %for computing J
+wh = w/h; %ratio that is useful
+J = w^3*h*(16/3-3.36*wh*(1-1/12*wh^4)); 
+C = (G*J./L_silicone)/(1000).^3; %J is in mm^4 and L is in mm, so I need 1000^(-3) to get to S.I units
+Factive = 0.015; %in N
+L = L_silicone/1000; %in m 
+sigma = Factive.*(L+0.045)./C %im not sure what L they use, it should be the center to center between two hexbugs, their
+%center to center is L + 0.045 (size of hexbug... I think).
